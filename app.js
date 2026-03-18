659203c6869fd0b2788bd349192ecac158f3dd37
temuulen-tech <baterdenezagdsuren024@gmail.com>
2026-03-18 18:09:29 +0800

    Extract safe app shell templates from index

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
  * Main bootstrap/controller that starts the existing app flow.
  */
 
-import { initializeApp } from "./script.js";
+import { mountAppShell } from "./render-shell.js";
+
+mountAppShell();
+
+const { initializeApp } = await import("./script.js");
 
 initializeApp();
